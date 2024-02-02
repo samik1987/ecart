5c82b67aedf9d02007272361b8e708a8f640a4b6
samik1987 <samik.net@gmail.com>
2024-02-02 18:48:00 +0530

    ecart first commit

---
+++ src/test/java/com/crafty/ecart/EcartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crafty.ecart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
